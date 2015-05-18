3de199876f061a7dd69b3dbe0ebc08279e116f44
Morgan Martin <morganm5201@gmail.com>
2015-05-18 03:09:16 +0000

    Setup Initial Board Class

---
+++ lib/board.rb
@@ -0,0 +1,68 @@
+require './piece'
+
+class Board
+  def initialize(board = empty_board)
+    @board = board
+  end
+
+  def empty_board
+    Array.new(8) { Array.new(8) }
+  end
+
+  def set_board
+    set_pawns
+    set_nobles
+  end
+
+  def set_pawns
+    @board[1].each_with_index do |cell, index|
+      @board[1][index] = Pawn.new([1, index], :white)
+    end
+    @board[6].each_with_index do |cell, index|
+      @board[6][index] = Pawn.new([6, index], :black)
+    end
+  end
+
+  def set_nobles
+    set_white_nobles
+    set_black_nobles
+  end
+
+  def set_white_nobles
+    @board[0].each_with_index do |cell, index|
+      case index
+      when 0, 7
+        @board[0][index] = Rook.new([0, index], :white)
+      when 1, 6
+        @board[0][index] = Knight.new([0, index], :white)
+      when 2, 5
+        @board[0][index] = Bishop.new([0, index], :white)
+      when 3
+        @board[0][3] = Queen.new([0, 3], :white)
+      when 4
+        @board[0][4] = King.new([0, 4], :white)
+      end
+    end
+  end
+
+  def set_black_nobles
+    @board[7].each_with_index do |cell, index|
+      case index
+      when 0, 7
+        @board[7][index] = Rook.new([7, index], :black)
+      when 1, 6
+        @board[7][index] = Knight.new([7, index], :black)
+      when 2, 5
+        @board[7][index] = Bishop.new([7, index], :black)
+      when 3
+        @board[7][3] = Queen.new([7, 3], :black)
+      when 4
+        @board[7][4] = King.new([7, 4], :black)
+      end
+    end
+  end
+end
+
+board = Board.new
+board.set_board
+puts board.inspect
